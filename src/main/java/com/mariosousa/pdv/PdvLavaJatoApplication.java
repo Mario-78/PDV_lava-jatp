bbe7c33d7b3a547d385bb86a26810fa1acf7f651
Mario-78 <mariojsws1@gmail.com>
2022-03-15 14:26:36 -0300

    First commit

---
+++ src/main/java/com/mariosousa/pdv/PdvLavaJatoApplication.java
@@ -0,0 +1,13 @@
+package com.mariosousa.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvLavaJatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvLavaJatoApplication.class, args);
+	}
+
+}
